5ec6fdb490789d16e1b6338b50a85920cd0c500e
almirashynybek <almirashynybek@gmail.com>
2025-02-16 23:31:09 +0500

    Added Backend folder

---
+++ Backend/django-demo/blog_project/blog_project/views.py
@@ -0,0 +1,11 @@
+# from django.http import HttpResponse, JsonResponse
+
+
+# def index(request):
+#     # print('#' * 100)
+#     # print(request.META)
+#     # print('#' * 100)
+#     return JsonResponse(data={'message': "Hello, world!"})
+
+# Posts
+# Comments
---
+++ Backend/django-demo/blog_project/posts/views.py
@@ -0,0 +1,9 @@
+from django.http import JsonResponse
+
+from .models import Post
+
+def index(request):
+
+    first_post = Post.objects.all().first()
+
+    return JsonResponse({'message': 'Hello, world!', 'title': first_post.title, 'body': first_post.body})
